a02d99af6b6e8e7971d8a6cd4d4fa5273b89a8e0
Amit Meena <cse160001004@iiti.ac.in>
2020-04-30 17:33:48 +0530

    Added Heart rate module (#8)
    * - Fixes and theme updates
    
    * HeartRate Page

--- lib/router.dart
+++ lib/router.dart
@@ -3,11 +3,13 @@ import 'package:openemr/interface/home/home.dart';
 import 'package:openemr/interface/login/login.dart';
 import 'package:openemr/interface/qrCode/qr_code.dart';
 import 'package:openemr/interface/patientList/patientList.dart';
+import 'package:openemr/interface/heartRate/heartRate.dart';
 
 final routes = {
   '/login':         (BuildContext context) => new LoginScreen(),
   '/home':          (BuildContext context) => new HomeScreen(),
   '/qrCode':        (BuildContext context) => new QrCode(),
   '/patientList':   (BuildContext context) => new PatientList(),
+  '/heartRate':   (BuildContext context) => new HeartRate(),
   '/' :             (BuildContext context) => new LoginScreen(),
 };
