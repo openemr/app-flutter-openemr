6fe3004e43f558687475c5b34a6a6b3e83e8539f
Amit Meena <amitmeena094@gmail.com>
2020-05-18 06:38:05 +0530

    coming soon added

---
+++ lib/interface/comingSoon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComingSoon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coming Soon")
+      ),
+      body: Center(
+        child: Text("Coming Soon"),
+      ),
+    );
+  }
+}
