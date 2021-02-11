1cff88c615c664a3fe3b69481d41a5e963f5c55b
viradhanus <e16083@eng.pdn.ac.lk>
2021-02-11 19:59:05 +0530

    pushAndRemoveUntil added

--- lib/screens/telehealth/profile.dart
+++ lib/screens/telehealth/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:openemr/screens/telehealth/telehealth.dart';
 import '../../models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -118,6 +119,7 @@ class _FirebaseProfileScreenState extends State<FirebaseProfileScreen> {
         .collection('username')
         .document(user.uid)
         .updateData({"name": _name});
-    Navigator.pop(context);
+    Navigator.of(context)
+        .pushAndRemoveUntil(MaterialPageRoute(builder: (context) => Telehealth()),(route)=>false);
   }
 }
